c7dfa7bc1536242c3fa9ac5b5214f7317b38f0ea
harshithm2508 <harshithm2508@gmail.com>
2024-05-14 23:19:51 +0530

    ButtonProps interface added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,13 @@
-function Button({text, bgColor, color, visibility, border} : {text : string, bgColor : string, color : string, visibility : boolean, border? : string}){
+interface ButtonProps{
+    text : string;
+    bgColor : string;
+    color : string;
+    visibility : boolean;
+    border? : string
+}
+
+
+function Button({text, bgColor, color, visibility, border} : ButtonProps){
     return(
         <button className={`${bgColor} ${color} ${visibility ? 'hidden sm:block' : null} border-2 ${border} px-4 py-2 font-semibold rounded-md`}>{text}</button>
     )
