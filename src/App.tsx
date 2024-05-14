dd1e3eb1feda386100ce0ffde49aed3b4fd7db70
harshithm2508 <harshithm2508@gmail.com>
2024-05-14 19:00:12 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Wallet from './pages/Wallet'
+
 
 function App() {
   return(
     <>
       <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signup' element={<SignUp/>}></Route>
+          <Route path='/wallet' element={<Wallet/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
