c9b5a96a28480fc04ea0149c72018e05696cc76a
harshithm2508 <harshithm2508@gmail.com>
2024-05-15 19:36:05 +0530

    corrected signup page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -9,31 +9,31 @@ export default function SignUp(){
         <div className="w-full h-screen-minus-20 flex justify-center">
             <div className="sm:w-1/2 min-w-0.5 h-1/2 mt-14 sm:mt-0">
                 <div className=" text-2xl font-semibold">Welcome to <span className=" text-blue-700">PixelPurse</span></div>
-                <div className="text-md font-medium text-gray-500">Log into your account using email or username</div>
+                <div className="text-md font-medium text-gray-500">Create a <span className="text-blue-700">PixelPurse</span> account</div>
 
-                <div className="mt-8 w-full">
+                <div className="mt-6 w-full">
                     <div className="text-md font-semibold">First Name</div>
                     <Input inputType="text" placeholder="John" width="w-full sm:w-9/12 md:w-8/12" height=" h-10" bgColor="bg-gray-100"/>
                 </div>
 
-                <div className="mt-8 w-full">
+                <div className="mt-6 w-full">
                     <div className="text-md font-semibold">Last Name</div>
                     <Input inputType="text" placeholder="Doe" width="w-full sm:w-9/12 md:w-8/12" height=" h-10" bgColor="bg-gray-100"/>
                 </div>
 
-                <div className="mt-8 w-full">
+                <div className="mt-6 w-full">
                     <div className="text-md font-semibold">Email or Username</div>
                     <Input inputType="text" placeholder="example@mail.com" width="w-full sm:w-9/12 md:w-8/12" height=" h-10" bgColor="bg-gray-100"/>
                 </div>
 
-                <div className="mt-8 w-full">
+                <div className="mt-6 w-full">
                     <div className="text-md font-semibold">Password</div>
                     <Input inputType="text" placeholder="" width="w-full sm:w-9/12 md:w-8/12" height=" h-10" bgColor="bg-gray-100"/>
                 </div>
-                <Button text="Login" margin="mt-6" bgColor="bg-blue-700" color="text-white" width="w-full sm:w-9/12 md:w-8/12"/>
+                <Button text="Sign Up" margin="mt-6" bgColor="bg-blue-700" color="text-white" width="w-full sm:w-9/12 md:w-8/12"/>
                 <div className="text-center">OR</div>
                 <Button text="Connect with Google" bgColor="bg-gray-200" color="text-gray-900" width="w-full sm:w-9/12 md:w-8/12"/>
-                <div className="mt-4 font-semibold text-center text-gray-500 mb-4">Already have an account ? <Link className=" text-blue-800" to={'/login'}>Sign Up</Link></div>
+                <div className="mt-4 font-semibold text-center text-gray-500 mb-4">Already have an account ? <Link className=" text-blue-800" to={'/login'}>Login</Link></div>
                 
             </div>
         </div>
