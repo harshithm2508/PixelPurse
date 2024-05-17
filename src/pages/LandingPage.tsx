300cf552bce4582f8f335bd50e43598bdd24ef6a
harshithm2508 <harshithm2508@gmail.com>
2024-05-17 22:52:52 +0530

    titled Landing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,16 @@
-function LandingPage(){
-    return(
-        <>
-            <h1>Landing Page</h1>
-        </>
-    )
+function LandingPage() {
+    return (
+        <div className="h-screen-minus-20 bg-blue-500 flex justify-center">
+            <div className="h-1/2 w-3/5 mt-16 flex flex-col items-center" id="in-box">
+                <div className=" text-center font-extrabold text-white text-6xl sm:text-8xl md:text-9xl mb-4">
+                    PixelPurse
+                </div>
+                <div className=" text-center font-extrabold sm:font-bold text-white text-xl sm:text-3xl md:text-3xl">
+                    Safeguarding Student Stashes
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LandingPage;
