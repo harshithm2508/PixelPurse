376765b32e70e2c4ede13674629dd897928f74bd
harshithm2508 <harshithm2508@gmail.com>
2024-05-23 23:09:43 +0530

    loggedIn loggedOut Nabvar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { useState } from "react";
 
 function Navbar(){
-    const navigate = useNavigate();
 
+    const navigate = useNavigate();
+    const [ loggedIn, setLogin] = useState(true);
 
     return(
         <div className="h-20 flex w-full items-center pl-5 sm:pl-24 pr-10 sm:pr-40 justify-between">
-            <div className=" font-medium text-2xl text-blue-700">PixelPurse</div>
-            <div className="flex gap-6">
-                <Button text="Login" onClickFunc={()=>{navigate('/login');}} bgColor="bg-white" color="text-black" visibility={false} border="border-blue-500"/>
-                <Button text="Sign Up" onClickFunc={()=>{navigate('/signup');}} bgColor="bg-blue-500" color="text-white" visibility={true} border="border-blue-500"/>
-            </div>
+            <div className=" font-medium text-2xl text-blue-700 cursor-pointer" onClick={()=>{loggedIn ? navigate('/dashboard') : navigate('/')}}>PixelPurse</div>
+            {(!loggedIn) ? <LoggedOutNavbar/> : <LoggedInNavbar/>}
         </div>      
+    )
+}
+
+function LoggedOutNavbar(){
+    const navigate = useNavigate();
+    return(
+        <div className="flex gap-6">
+            <Button text="Login" onClickFunc={()=>{navigate('/login');}} bgColor="bg-white" color="text-black" visibility={false} border="border-blue-500"/>
+            <Button text="Sign Up" onClickFunc={()=>{navigate('/signup');}} bgColor="bg-blue-500" color="text-white" visibility={true} border="border-blue-500"/>
+        </div>
+    )
+}
+
+function LoggedInNavbar(){
+    const navigate = useNavigate();
+    return(
+        <div className="flex gap-6">
+            <Button text="Home" onClickFunc={()=>{navigate('/wallet');}} bgColor="bg-white" color="text-black" visibility={false} border="border-white"/>
+            <Button text="Wallet" onClickFunc={()=>{navigate('/dashboard');}} bgColor="bg-white" color="text-black" visibility={true} border="border-white"/>
+            <Button text="My Profile" onClickFunc={()=>{navigate('/dashboard');}} bgColor="bg-white" color="text-black" visibility={true} border="border-white"/>
+        </div>
     )
 }
 
