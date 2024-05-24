d2b7fa8a90fadc3c45d16d1fd8c6c64decf967a7
harshithm2508 <harshithm2508@gmail.com>
2024-05-24 23:34:30 +0530

    removed test login button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,6 @@ function Navbar(){
         <div className="h-20 flex w-full items-center pl-5 sm:pl-24 pr-10 sm:pr-40 justify-between">
             <div className=" font-medium text-2xl text-blue-700 cursor-pointer" onClick={()=>{loggedIn ? navigate('/dashboard') : navigate('/')}}>PixelPurse</div>
             {(!loggedIn) ? <LoggedOutNavbar/> : <LoggedInNavbar/>}
-            <button onClick={()=>{setLogin(true)}}>login</button>
         </div>      
     )
 }
