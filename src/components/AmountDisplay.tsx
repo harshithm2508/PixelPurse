525a18a72c2c250e69974fd6f5790f80b9682197
harshithm2508 <harshithm2508@gmail.com>
2024-06-05 13:12:08 +0530

    feature to Add Money

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -5,7 +5,7 @@ interface AmountDisplayProps{
 
 export function AmountDisplay({description, amount} : AmountDisplayProps){
     return(
-        <div className=" w-5/12 min-w-44 border-2 border-gray-300 rounded-lg ">
+        <div className=" w-1/2 min-w-44 border-2 border-gray-300 rounded-lg ">
             <div className=" font-bold text-2xl"><span>&#8377;</span>  {amount}</div>
             <div className=" text-base font-medium text-gray-500">{description}</div>
         </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { useRecoilState, useRecoilValue } from "recoil"
 import { userData } from "../state/atoms/UserInfo"
 import { WalletBalance, TotalTransactions } from "../state/atoms/MonetaryState";
 import { AmountDisplay } from "../components/AmountDisplay";
+import Button from "../components/Button";
 
 export default function Dashboard(){
 
@@ -17,17 +18,24 @@ export default function Dashboard(){
             <div className=" mt-10 text-5xl text font-good">Welcome back, {first_name}</div>
 
             {/* Total Transactions and Wallet balance */}
-            <div className=" mt-5 w-full flex flex-wrap gap-10 text-center">
+            <div className=" mt-5 w-full flex flex-wrap text-center">
                 {/* Wallet Balance  */}
                 <AmountDisplay amount={walletBal} description="Current Balance"/>
                 <AmountDisplay amount={totalTransactions} description="Total Transactions"/>
-
-
             </div>
 
 
-
-            <div>add money</div>
+            {/* Add Money  */}
+            <div className=" mt-5 w-full border-2 border-gray-300 px-5 py-2 flex justify-between items-center rounded-lg">
+                <div>
+                    <div className=" font-bold">Add Money</div>
+                    <div className="text-base font-medium text-gray-500">Use your bank account or debit card to add money to your <span className="text-blue-700">PixelPurse</span> wallet</div>
+                </div>
+                <div>
+                    <Button bgColor="bg-blue-700" text="Add money" color="text-white"/>
+                </div>
+            </div>
+            
             <div> Recent transactions</div>
         </div>
     )
