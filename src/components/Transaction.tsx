ca00b223728377cfd2966d339559bd060d47e58c
harshithm2508 <harshithm2508@gmail.com>
2024-06-05 19:30:17 +0530

    set user display name in transaction list

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -7,10 +7,11 @@ interface TransactionsProps{
 
 
 export function TransactionsList({name, process, amount, date} : TransactionsProps){
+    const naam = name.split(' ');
     return(
         <div className=" flex border-b-2 justify-between items-center p-2">
             <div className=" flex gap-5">
-                <div className=" bg-blue-300">IMG</div>
+                <div className=" bg-blue-300 rounded-full h-10 w-10 flex justify-center items-center">{naam[0][0]+naam[1][0]}</div>
                 <div>
                     <div className=" font-mukta">You {process}  <span>&#8377;</span>{amount}  {process == 'sent' ? 'to' : 'from'} {name}</div>
                     <div className=" text-gray-400 text-sm font-semibold">{date}</div>
