452b119a759706fde218597fc013fe75f6d629ee
harshithm2508 <harshithm2508@gmail.com>
2024-06-16 23:45:24 +0530

    set 'lg' and 'xl' screens

--- src/components/PaymentOptions.tsx
+++ src/components/PaymentOptions.tsx
@@ -5,7 +5,7 @@ interface PaymentOptionProps{
 
 export default function PaymentOption({nameOfCard, cardNumber} : PaymentOptionProps){
     return(
-        <div className=" border-b-2 rounded-lg pr-2 lg:pr-20 pt-2 pl-4 pb-2 mb-2 flex justify-between">
+        <div className=" border-b-2 rounded-lg pr-4 xl:pr-14 2xl:pr-18 pt-2 pl-4 pb-2 mb-2 flex justify-between">
             <div className=" flex gap-4 lg:gap-10">
                 <div>{nameOfCard}</div>
                 <div>{cardNumber}</div>
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -14,13 +14,13 @@ export default function Wallet(){
 
 
     return(
-        <div className=" w-full sm:h-auto bg-white lg:bg-blue-500 flex justify-center items-center">
-            <div className=" w-full w-full lg:w-4/5 h-full sm:h-auto px-10 py-3 bg-white rounded-xl flex flex-col gap-5">
+        <div className=" w-full sm:h-auto lg:h-screen-minus-20 bg-white lg:bg-blue-500 flex justify-center items-center">
+            <div className=" w-full lg:w-4/5 2xl:w-4/5 lg:p-8 h-full sm:h-auto px-10 py-3 bg-white rounded-xl flex flex-col gap-5">
 
                 {/* Introduction to wallet and button to check your transactions */}
-                <div className=" flex justify-between">
+                <div className=" flex justify-between flex-wrap">
                     <div className=" font-bold text-2xl flex items-center">Your Wallet</div>
-                    <Button text="See your transactions" bgColor="bg-gray-200" color="black" border="rounded-xl" misc=" hidden sm:visible"/>
+                    <Button text="See your transactions" bgColor="bg-gray-200" color="black" border="rounded-xl" misc=" mt-2 " />
                 </div>
 
 
@@ -33,7 +33,7 @@ export default function Wallet(){
 
 
                 {/* Adding Funds */}
-                <div className="w-full border-2 border-gray-300 px-5 py-4 flex flex-col-reverse sm:flex-row items-center justify-between lg:justify-center gap-5 rounded-lg">
+                <div className="w-full border-2 border-gray-300 px-5 py-4 flex flex-col-reverse sm:flex-row items-center justify-between gap-5 rounded-lg">
                     <div>
                         <div className=" font-bold">Add Money</div>
                         <div className="text-base font-medium text-gray-500">Use your bank account or debit card to add money to your <span className="text-blue-700">PixelPurse</span> wallet</div>
@@ -48,7 +48,7 @@ export default function Wallet(){
                 <div className=" h-auto w-full border-blue-500 flex flex-col sm:flex-row gap-8">
 
                     {/* Quick Actions Section */}
-                    <div className=" w-full lg:w-2/5 p-5 border-gray-300 border-2 rounded-lg">
+                    <div className=" w-full lg:w-1/2 p-5 flex flex-col justify-between border-gray-300 border-2 rounded-lg">
                         <div className=" font-bold text-lg">Quick Actions</div>
                         <QuickActionBar title="Pay or Request"/>                        
                         <QuickActionBar title="Invite Friends"/>                        
@@ -58,7 +58,7 @@ export default function Wallet(){
 
 
                     {/* Payment Methods  */}
-                    <div className=" w-full lg:w-2/5 p-5 border-gray-300 border-2 rounded-lg">
+                    <div className=" w-full lg:w-1/2 p-5 border-gray-300 border-2 rounded-lg">
                         <div className=" font-bold text-lg">Payment Methods</div>
                         <PaymentOption nameOfCard="Mastercard" cardNumber="5149"/>
                         <PaymentOption nameOfCard="Visa" cardNumber="1753"/>
